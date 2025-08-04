3f38ba96be3ae9b85a2749776f822b39b2ac7700
Mason Day <daymason@hotmail.co.uk>
2025-08-04 13:15:00 +0100

    WIP

--- resources/js/ssr.ts
+++ resources/js/ssr.ts
@@ -3,7 +3,6 @@ import createServer from '@inertiajs/vue3/server'
 import { renderToString } from '@vue/server-renderer'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import { createSSRApp, DefineComponent, h } from 'vue'
-import { ZiggyVue } from 'ziggy-js'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
 
