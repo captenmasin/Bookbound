f347b1c5a8189b5f36960fb6391509211424dbbb
Mason Day <daymason@hotmail.co.uk>
2025-08-09 17:24:12 +0100

    WIP

--- resources/js/pages/books/Show.vue
+++ resources/js/pages/books/Show.vue
@@ -32,7 +32,7 @@ const props = defineProps({
 })
 
 const { updateSingleSetting, getSingleSetting } = useUserSettings()
-const { authed } = useAuthedUser()
+const { authed, authedUser } = useAuthedUser()
 
 const data = [
     {
@@ -98,6 +98,7 @@ defineOptions({
                                         :src="book.cover" />
                                 </div>
                             </UpdateBookCover>
+                            {{ authedUser?.subscription }}
                         </div>
                         <div class="flex flex-1 justify-between w-full md:hidden">
                             <div class="flex flex-col gap-1 flex-1">
