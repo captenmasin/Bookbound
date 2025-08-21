221a4ce65fbd1d93023827f92e2ff19122886c48
Mason Day <daymason@hotmail.co.uk>
2025-08-21 22:14:35 +0100

    WIP

--- resources/js/components/AppHeader.vue
+++ resources/js/components/AppHeader.vue
@@ -122,7 +122,7 @@ const isDesktop = useMediaQuery('(min-width: 768px)')
 
 <template>
     <div
-        class="sticky safe-top safe-h-14 z-50 md:h-16  border-b border-sidebar-border/80 transition-all duration-300 ease-in-out bg-background md:static md:translate-y-0"
+        class="sticky safe-top safe-h-14 z-50 md:h-16 safe-pt border-b border-sidebar-border/80 transition-all duration-300 ease-in-out bg-background md:static md:translate-y-0"
         :class="{ '-NOT-translate-y-full': !isVisible }">
         <div class="mx-auto flex items-center h-full px-4 pwa:md:max-w-none md:max-w-7xl">
             <div
