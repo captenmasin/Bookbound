fc683f6cfbb6e4f4692df665619e9d2fea8c4923
Mason Day <daymason@hotmail.co.uk>
2025-08-22 08:43:26 +0100

    WIP

--- resources/js/components/ui/button/index.ts
+++ resources/js/components/ui/button/index.ts
@@ -12,7 +12,7 @@ export const buttonVariants = cva(
                 destructive:
                     'bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
                 'destructive-ghost':
-                    'bg-destructive/10 text-destructive shadow-xs hover:bg-destructive/20 focus-visible:ring-destructive/30 dark:focus-visible:ring-destructive/30 dark:bg-destructive/60',
+                    'bg-destructive/10 text-destructive shadow-xs hover:bg-destructive/20 focus-visible:ring-destructive/30 dark:focus-visible:ring-destructive/30 dark:bg-destructive/30',
                 'destructive-outline':
                     'border bg-transparent shadow-xs text-destructive hover:bg-destructive hover:text-white border-destructive dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
                 outline:
