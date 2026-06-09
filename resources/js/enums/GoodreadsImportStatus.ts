ac0abba23a3a67a7b9356c9bff5faaaeb7e27292
Mason Day <daymason@hotmail.co.uk>
2026-06-09 21:36:04 +0100

    WIP

--- resources/js/enums/GoodreadsImportStatus.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-unused-vars */
-export enum GoodreadsImportStatus {
-Pending = 'pending',
-Processing = 'processing',
-Completed = 'completed',
-Failed = 'failed'
-}
-/* eslint-disable no-unused-vars */
